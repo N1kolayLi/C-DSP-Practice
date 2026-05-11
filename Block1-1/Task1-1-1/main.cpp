a857a10404407a778d6a97a2e5c3cd6b01636c83
N1kolayLi <nik.li.kvpo@gmail.com>
2026-05-11 18:19:54 +0700

    Описание задачи

--- Block1-1/Task1-1-1/main.cpp
+++ Block1-1/Task1-1-1/main.cpp
@@ -1,12 +1,30 @@
 #include <iostream>
 #include <windows.h>
-
+using namespace std;
 //Требуется создать набор функций.
 
 /*Дана последовательность вещественных чисел произвольной длины.
  * Последовательность хранится в памяти как статический массив.
  * Размер последовательности заранее неизвестен и передается в функции как параметр.*/
 
+
+
 int main(){
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+    //Массив
+    int n;
+    cout<<"Введите количество элементов массива"<<endl;
+    scanf("%d", &n);
+
+    double arr[n];
+
+        for(int i = 0; i < n; i++){
+            cout<<"Эллемент " << i << " = " <<endl;
+            scanf("%lf", &arr[i]);
+        }
+        for(int i = 0; i < n; i++){
+            cout<<arr[i]<<" "<<endl;
+        }
 
 }
