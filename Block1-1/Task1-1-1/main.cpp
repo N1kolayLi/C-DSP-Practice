f596aaaf21cdc1b771fe6edcdf98e00af86b0ca3
N1kolayLi <nik.li.kvpo@gmail.com>
2026-05-13 17:14:29 +0700

    Начало работы над функцией с экстремумами, нормально передается массив и размер в функцю

--- Block1-1/Task1-1-1/main.cpp
+++ Block1-1/Task1-1-1/main.cpp
@@ -1,17 +1,19 @@
 #include <iostream>
 #include <windows.h>
 using namespace std;
+#define MAX_SIZE 100
 //Требуется создать набор функций.
 
 /*Дана последовательность вещественных чисел произвольной длины.
  * Последовательность хранится в памяти как статический массив.
  * Размер последовательности заранее неизвестен и передается в функции как параметр.*/
 
+int size = 0;
 void func_arr(){
     int n;
     cout << "Введите количество элементов массива: ";
     scanf("%d", &n);
-
+    size = n;
     double arr[n];
 
     for(int i = 0; i < n; i++) {
@@ -31,12 +33,19 @@ void func_arr(){
 // Локальный экстремум — это элемент, который больше (для максимума)
 // или меньше (для минимума) обоих своих соседей.
 // Крайние элементы последовательности (первый и последний) экстремумами не считаются.
-void find_local_extr(const double *arr){
-
-
+void find_local_extr(const double *arr, int size){
+    cout<<size<<endl;
 }
+
 int main(){
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
+
+    double my_array[MAX_SIZE];
+
     func_arr();
+    if (size > 0) {
+        find_local_extr(my_array, size);
+    }
+return 0;
 }
